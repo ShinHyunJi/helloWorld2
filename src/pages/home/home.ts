7560cbac555f4628b500b7fdf1ef1594cc867d01
hjshin612 <hjshin612@naver.com>
2018-12-05 22:06:00 +0900

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,67 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+        //이미 내장되어 있는 컨트롤러 
+import { NavController, AlertController } from 'ionic-angular';
+import * as firebase from "firebase";
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+export class HomePage {
+                    //any? java의 generic처리 
+  private userName : any;
+  private userEmail : any;
+  private userId : any;
+
+  constructor(public navCtrl: NavController,
+    private alerCtrl : AlertController
+    ) {
+    this.initPage();
+  }
+
+  initPage(){
+    var user = firebase.auth().currentUser;
+    console.log(user);
+
+    if(user){           //firebase내부 정해진 명
+      this.userName = user.displayName;
+      this.userEmail = user.email;
+      this.userId = user.uid;
+    }else{
+      console.log('로그인 된 사용자가 없습니다');
+    }
+  }
+
+  logout(){   //this.가 붙는 건 무조건 생성자 확인! 
+                                        //JSON객체 
+    let confirm = this.alerCtrl.create({
+      title: '로그아웃',
+      message : '로그아웃하시겠습니까?',
+      buttons : [{                   //배열 안에 json객체 2개 있는 형태
+        text:'아니오',
+        handler: ()=>{     //function은 error! 람다
+          console.log('Logout 취소');
+        } 
+      },{
+        text : '예',
+        handler: ()=>{
+          console.log('Logout 확인');
+
+          firebase.auth().signOut()
+          .then(()=>{     //타입스크립트&람다식
+            console.log('로그아웃 실행');
+          })
+          .catch((error)=>{
+            console.log(error)});
+        }
+      }]      
+    });
+    confirm.present();
+
 
   }
+
 
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,30 +1,31 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-import * as firebase from "firebase";
 import { SignupPage } from '../signup/signup';
+import { LoaderProvider } from '../../providers/loader/loader';
+import * as firebase from "firebase";
 
 /**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+* Generated class for the LoginPage page.
+*
+* See https://ionicframework.com/docs/components/#navigation for more info on
+* Ionic pages and navigation.
+*/
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+selector: 'page-login',
+templateUrl: 'login.html',
 })
 export class LoginPage {
-
   private account : any ={
-    email : '',
-    password : ''
+  email : '',
+  password : ''
   };
 
   //생성자  instance:type
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+    //private loader: LoaderProvider,
+    public navParams: NavParams) {
   }
 
   //catch
@@ -32,12 +33,26 @@ export class LoginPage {
     console.log('ionViewDidLoad LoginPage');
   }
 
-  login(){
-  }
+  login(){          //promise Pattern 
+
+    //this.loader.show();
+    firebase.auth().signInWithEmailAndPassword(this.account.email, this.account.password)
+    .then((result)=>{
+      console.log(result);
+    })
+
+    .catch((error) =>{
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorMessage);
+    });
+   //this.loader.hide();
+    }
 
   //signup 페이지로 이동
   signup(){
     this.navCtrl.push(SignupPage);
-  }
-
-}
+    }
+     
+    }
